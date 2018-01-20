8c28a359c65fb9e90f2e01f7f4669756a5000bde
Jerry Chen <jchen0022@berkeley.edu>
2018-01-20 15:15:10 -0800

    more stuff

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,5 +1,30 @@
 class CirclesController < ApplicationController
 
-    def new
+  def new
+  end
+
+  def show
+    @user = current_user
+    @circle = current_circle
+
+    if not in_circle?
+      redirect_to root_path and return
     end
+    @events = @circle.events
+    @users = @circle.users
+
+  end
+
+  def in_circle?
+    if not current_circle
+      # The circle does not exist
+      redirect_back fallback_location: root_path and return
+    end
+
+    return current_circle.users.include? current_user
+  end
+
+  def current_circle
+    Circle.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,8 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, :print_hello
-
-  def print_hello
-  end
 
   def dashboard
-    if 
-    end
     @user = current_user 
   end
 
-  def show
-    @user = current_user
-    @circle = current_circle
-
-    if not in_circle?
-      redirect_to root_path and return
-    end
-    @events = @circle.events
-    @users = @circle.users
-
-    @user
-  end
-
-  def in_circle?
-    if not current_circle
-      # The circle does not exist
-      redirect_back fallback_location: root_path and return
-
-    return current_circle.users.include? current_user
-  end
-
-  def current_circle
-    Circle.find(params[:id])
-  end
 
 end
