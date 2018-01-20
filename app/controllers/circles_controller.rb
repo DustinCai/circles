52847b90306aea56cb6a673471acd511fb59e412
jenyeu <jenyeuw@gmail.com>
2018-01-20 14:37:38 -0800

    merge

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,2 +1,5 @@
 class CirclesController < ApplicationController
+
+    def new
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :print_hello
+
+  def print_hello
+  end
 
   def dashboard
+    if 
+    end
     @user = current_user 
   end
 
